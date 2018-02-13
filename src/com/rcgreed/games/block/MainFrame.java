0fe114fe5352ab80c4546915170d7ec03fdb8ffe
wang gang on notebook <wang.notebook.gang@gmail.com>
2018-02-13 11:25:49 +0800

    animation sample

--- src/com/rcgreed/games/block/MainFrame.java
+++ src/com/rcgreed/games/block/MainFrame.java
@@ -2,13 +2,16 @@ package com.rcgreed.games.block;
 
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.Timer;
 
 public class MainFrame extends JFrame {
 	/**
@@ -18,29 +21,50 @@ public class MainFrame extends JFrame {
 	private JLabel statusBar=new JLabel("game start");
 
 	public MainFrame() {
-		super("game");
-		getContentPane().add(new Trap(), BorderLayout.CENTER);
+		super("游戏");
+		Trap trap=new Trap();
+		Timer timer=new Timer(250,trap);
+		getContentPane().add(trap, BorderLayout.CENTER);
 		getContentPane().add(statusBar,BorderLayout.PAGE_END);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setJMenuBar(createMenuBar());
+		setJMenuBar(new MainMenuBar(this::newGame,new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if(timer.isRunning()) 
+					timer.stop();
+				else
+					timer.start();
+				
+			}
+		
+			
+		}));
 		setSize(500, 540);
-		setLocationRelativeTo(null);// center main frame of screen
+		setLocationRelativeTo(null);// center main frame on the screen
+		setResizable(false);
+		this.addKeyListener(trap);
+		
 	}
-	private JMenuBar createMenuBar() {
-		JMenuBar menuBar=new JMenuBar();
-		JMenu menuFile=new JMenu("Game");
-		JMenu menuOption=new JMenu("Option");
-		menuBar.add(menuFile);
-		menuBar.add(menuOption);
-		menuFile.setMnemonic(KeyEvent.VK_G);
-		menuOption.setMnemonic(KeyEvent.VK_O);
-		menuFile.getAccessibleContext().setAccessibleDescription("File menu");
-		JMenuItem menuItem=new JMenuItem("New Game",KeyEvent.VK_N);
-		menuFile.add(menuItem);
-		menuItem.addActionListener(this::newAction);
-		return menuBar;
+	private void newGame(ActionEvent e) {
+		statusBar.setText(" New Game");
 	}
-	private void newAction(ActionEvent e) {
-		statusBar.setText("New selected!");
+	private static class MainMenuBar extends JMenuBar{
+		public MainMenuBar(ActionListener newGame,ActionListener pauseGame) {
+			super();
+			JMenu menuFile=new JMenu("Game");
+			JMenu menuOption=new JMenu("Option");
+			add(menuFile);
+			add(menuOption);
+			menuFile.setMnemonic(KeyEvent.VK_G);
+			menuOption.setMnemonic(KeyEvent.VK_O);
+			menuFile.getAccessibleContext().setAccessibleDescription("File menu");
+			JMenuItem menuItem=new JMenuItem("New Game",KeyEvent.VK_N);
+			JMenuItem menuPause=new JMenuItem("Pause",KeyEvent.VK_P);
+			menuFile.add(menuItem);
+			menuFile.add(menuPause);
+			menuItem.addActionListener(newGame);
+			menuPause.addActionListener(pauseGame);
+		}
 	}
 }
--- src/com/rcgreed/games/block/Trap.java
+++ src/com/rcgreed/games/block/Trap.java
@@ -6,23 +6,67 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 
 import javax.swing.JPanel;
 
-public class Trap extends JPanel {
+public class Trap extends JPanel implements KeyListener,ActionListener {
 	/**
 	 * 
 	 */
+	final private static int GAP=2;
 	private static final long serialVersionUID = 1L;
+	final private static int MX = 10;
+	final private static int MY = 25;
+	final private boolean[][] matrix=new boolean[MY][MX];
+	public Trap() {
+		super();
+		for(int x=0;x<MX;x++) {
+			for(int y=0;y<MY;y++) {
+				matrix[y][x]=false;
+			}
+		}
+	}
 
 	@Override
 	protected void paintComponent(Graphics g) {
-		// TODO Auto-generated method stub
 		super.paintComponent(g);
-		drawDonut((Graphics2D) g);
+		Graphics2D g2d=(Graphics2D) g;
+		//drawDonut(g2d);
+		drawMatrix(g2d);
 	}
+
+	private void drawMatrix(Graphics2D g2d) {
+		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+		rh.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+
+		g2d.setRenderingHints(rh);
+
+		Dimension size = getSize();
+		double w = size.getWidth();
+		double h = size.getHeight();
+		int boxHeight=(((int)h-GAP)-MY*GAP)/MY;
+		int xstart=((int)w - (boxHeight + GAP)*10)/2;
+		g2d.setColor(Color.GRAY);
+		for(int x=0;x<MX;x++) {
+			for(int y=0;y<MY;y++) {
+				if(matrix[y][x])
+					g2d.fillRect(xstart+x*(boxHeight+GAP),GAP+y * (boxHeight+ GAP), boxHeight, boxHeight);
+				else
+					g2d.drawRect(xstart+x*(boxHeight+GAP),GAP+y * (boxHeight+ GAP), boxHeight, boxHeight);
+				
+			}
+		}
+		
+		
+	}
+	private int pos;
 
 	private void drawDonut(Graphics2D g2d) {
 
@@ -45,5 +89,36 @@ public class Trap extends JPanel {
 			at.rotate(Math.toRadians(deg));
 			g2d.draw(at.createTransformedShape(e));
 		}
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		repaint();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	// for timer use
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		int x=pos % MX;
+		int y=pos / MX;
+		matrix[y][x]=false;
+		pos++;
+		if(pos >=MY*MX)pos=0;
+		x=pos % MX;
+		y=pos / MX;
+		matrix[y][x]=true;
+		repaint();
 	}
 }
